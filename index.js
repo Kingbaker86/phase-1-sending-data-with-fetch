782c612004621f251a9ea8c89f2539e100f7db52
odaney baker <bakerobtb@gmail.com>
2023-04-23 21:41:39 -0400

    Done

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Add your code here
+function submitData(name, email) {
+    let formData = {
+      name: name,
+      email: email
+    };
+  
+    let configObject = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+      },
+      body: JSON.stringify(formData)
+    };
+  
+    return fetch("http://localhost:3000/users", configObject)
+      .then(response => response.json())
+      .then(data => {
+        let id = data.id;
+        document.body.append(id);
+      })
+      .catch(error => {
+        document.body.append(error.message);
+      });
+  }
+       
